c24b305ffc1172fd95bb06bcd31fded8870219e0
jakoblandbrecht <jakob.landbrecht@tum.de>
2026-03-06 13:51:49 +0100

    change URL

--- src/Application/World/MonitorScreen.ts
+++ src/Application/World/MonitorScreen.ts
@@ -184,7 +184,7 @@ export default class MonitorScreen extends EventEmitter {
 
         // Set iframe attributes
         // PROD
-        iframe.src = 'http://localhost:3000/';
+        iframe.src = 'https://portfolio-inner-site-ecru.vercel.app/events';
         /**
          * Use dev server is query params are present
          *
